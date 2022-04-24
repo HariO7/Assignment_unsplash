5936628ce08b87cf93256b63e50824768dd6bcad
hari07 <hariodukkathil@gmail.com>
2022-04-24 10:56:00 +0530

    backend setup

--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,14 @@
 require("dotenv").config();
 const express = require("express");
+const bodyParser = require("body-parser");
+const cors = require("cors");
 const app = express();
+const { pool } = require("./database/database.modal");
 
 const port = process.env.PORT;
+
+app.use(cors());
+app.use(bodyParser.json());
 
 app.get("/", (req, res) => {
   res.send("hello");
